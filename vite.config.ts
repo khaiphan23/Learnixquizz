0ea3a47e863e5d755ff1bf4b128d2805269caea8
khaiphan23 <khaidatrang@gmail.com>
2026-04-20 02:11:39 +0700

    chore: increase chunkSizeWarningLimit to 1000KB

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Tăng giới hạn cảnh báo lên 1000KB (1MB)
+  },
 });
